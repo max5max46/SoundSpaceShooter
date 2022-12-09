f2035b4634c929fbacd0dbc11617c8eb1e1da381
Dylan Adams <113365542+max5max46@users.noreply.github.com>
2022-12-09 08:57:41 -0400

    Initial commit

---
+++ Assets/Scripts/AsteroidDestroy.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class AsteroidDestroy : MonoBehaviour
+{
+    public GameObject explosion;
+    //public GameObject playerExplosion;    
+
+    private SFXManager sfxManager;
+    private GameManager gameManager;
+    private GameObject player;
+
+    void Awake()
+    {
+        sfxManager = ( GameObject.Find("SFXManager").GetComponent<SFXManager>() );
+        gameManager = ( GameObject.Find("GameManager").GetComponent<GameManager>() );
+        player = GameObject.Find("Player");
+    }
+
+    void OnTriggerEnter(Collider other)
+    {   
+        
+                     
+        if (other.tag == "Player")
+        {
+            Instantiate(explosion, this.transform.position, this.transform.rotation);
+            sfxManager.AsteroidExplosion();
+            Destroy(this.gameObject);            
+        }
+                  
+
+        if (other.tag == "Bullet")
+        {
+            gameManager.score = gameManager.score += 10;
+            Instantiate(explosion, this.transform.position, this.transform.rotation);
+            Destroy(this.gameObject);
+            //Debug.Log("killed by bullet");
+            sfxManager.AsteroidExplosion();
+            Destroy(other.gameObject);
+        }
+
+        if (other.tag == "Boundary")
+        {
+            Destroy(this.gameObject);
+        }
+
+
+
+
+    }
+}
